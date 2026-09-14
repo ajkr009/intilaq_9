2abd31c9e45ba91f3ed6ebeb7a0502010a276137
ajkr <gpp9581@gmail.com>
2026-09-14 16:00:58 -0700

    added all project files

--- intilaq_9/intilaq_9/urls.py
+++ intilaq_9/intilaq_9/urls.py
@@ -1,16 +1,7 @@
-import sys
-import os
 from django.contrib import admin
 from django.urls import path, include
 
-# പൈത്തൺ പാത്തിലേക്ക് ഈ ഫോൾഡർ കൂടി ചേർക്കുന്നു (ഇത് എറർ വരുന്നത് തടയും)
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-# ഇനി നേരിട്ട് views ഇമ്പോർട്ട് ചെയ്യാം
-import views
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('fun/', views.fun_page, name='fun_page'),
+    path('admin/',admin.site.urls),
+    path('',include('home.urls'))
 ]
